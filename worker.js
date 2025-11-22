e7216c3da8229b617918d9d3cf0374f2a52e43db
NamelessNanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-11-21 22:50:22 -0600

    Update license version to NNCL v1.2 in relevant files

--- worker.js
+++ worker.js
@@ -1,4 +1,4 @@
-// This software uses NNCL v1.1 see LICENSE.md for more info
+// This software uses NNCL v1.2 see LICENSE.md for more info
 // NamelessTelemetry Cloudflare Worker
 // - POST /census: accept minimal JSON payload from clients
 //   { id: sha256, date: YYYY-MM-DD, projectname|project: string }
